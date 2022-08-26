d5c20b98abaec8d9514d6cb3c31a876e29c57063
Balaji Venkatachalam <balaji.venkatachalam@bitflyer.com>
2022-08-26 19:05:12 +0900

    Added working code and commented it

---
+++ CSharp/DI/AzureFunctions-Bug/ApplicationDbContext/ApplicationContext-Working.cs
@@ -0,0 +1,16 @@
+﻿//using Microsoft.EntityFrameworkCore;
+
+//namespace ApplicationDbContext
+//{
+//    public class ApplicationContext : DbContext
+//    {
+//        public ApplicationContext(DbContextOptions<ApplicationContext> connectionOptions) : base(connectionOptions)
+//        {
+//        }
+//        public ApplicationContext(string connectionString) : this(new DbContextOptionsBuilder<ApplicationContext>().UseSqlServer(connectionString).Options)
+//        {
+//        }
+//    }
+//}
+
+
